78e92ac194cfdcf5fb1bf357ff19415c77233224
Brittany Gollins and Kimberly McConnell <kimberly@>
2018-09-12 19:48:41 -0700

    add Multiplication form sections to index.html and front-end logic for Division AND Multiplication functionality

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,22 @@ $(document).ready(function() {
     var number1 = parseInt($("#num1").val());
     var number2 = parseInt($("#num2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#additionoutput").text(result);
+ });
+
+ $("form#division").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#divide1").val());
+   var number2 = parseInt($("#divide2").val());
+   var result = divide(number1, number2);
+   $("#divisionoutput").text(result);
+ });
+
+ $("form#multiply").submit(function(event) {
+   event.preventDefault();
+   var number1 = parseInt($("#multiply1").val());
+   var number2 = parseInt($("#multiply2").val());
+   var result = multiply(number1, number2);
+   $("#multiplicationoutput").text(result);
  });
 });
