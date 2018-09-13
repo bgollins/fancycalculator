7b30bafa243027639aac07b877207667408f7e66
Brittany Gollins and Kimberly McConnell <brittany@>
2018-09-12 18:53:48 -0700

    add initial business logic to scripts.js and basic HTML for user input capability

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+// Business logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is front-end logic:
+
+$(document).ready(function(event) {
+
+});
